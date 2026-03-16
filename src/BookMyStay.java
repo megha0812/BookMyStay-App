d2eb0cea7fb6cf199425598350f744d7e9857d00
megha0812 <meghakadiyala12@gmail.com>
2026-03-16 10:32:56 +0530

    megha kadiyala added UC1

---
+++ src/BookMyStay.java
@@ -0,0 +1,15 @@
+//version 1.0
+//usecase 1:  Application Entry & Welcome Message
+public class HotelApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" Welcome to Hotel Booking System ");
+        System.out.println(" Version: 1.0 ");
+        System.out.println("=================================");
+
+        System.out.println("Application started successfully.");
+        System.out.println("Application terminated.");
+    }
+}
