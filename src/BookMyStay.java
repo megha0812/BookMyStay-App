195fbe762a6821b94a7e17b33de7999605d95c7b
megha kadiyala <meghakadiyala12@gmail.com>
2026-03-24 10:20:48 +0530

    megha kadiyala added UC7

--- src/BookMyStay.java
+++ src/BookMyStay.java
@@ -1,15 +1,63 @@
-//version 1.0
-//usecase 1:  Application Entry & Welcome Message
-public class HotelApp {
+//version 7.0
+//usecase 7:  Add-On Service Selection
+import java.util.*;
 
+class AddOnService {
+    String name;
+    int cost;
+
+    public AddOnService(String name, int cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+}
+
+class AddOnServiceManager {
+    private Map<String, List<AddOnService>> serviceMap = new HashMap<>();
+
+    public void addService(String reservationId, AddOnService service) {
+        serviceMap.putIfAbsent(reservationId, new ArrayList<>());
+        serviceMap.get(reservationId).add(service);
+
+        System.out.println("Added service: " + service.name);
+    }
+
+    public int calculateTotal(String reservationId) {
+        int total = 0;
+        List<AddOnService> services = serviceMap.get(reservationId);
+
+        if (services != null) {
+            for (AddOnService s : services) {
+                total += s.cost;
+            }
+        }
+        return total;
+    }
+
+    public void showServices(String reservationId) {
+        List<AddOnService> services = serviceMap.get(reservationId);
+
+        System.out.println("\nServices for " + reservationId + ":");
+        if (services != null) {
+            for (AddOnService s : services) {
+                System.out.println(s.name + " - " + s.cost);
+            }
+        }
+    }
+}
+
+public class UseCase7 {
     public static void main(String[] args) {
+        AddOnServiceManager manager = new AddOnServiceManager();
 
-        System.out.println("=================================");
-        System.out.println(" Welcome to Hotel Booking System ");
-        System.out.println(" Version: 1.0 ");
-        System.out.println("=================================");
+        String reservationId = "R101";
 
-        System.out.println("Application started successfully.");
-        System.out.println("Application terminated.");
+        manager.addService(reservationId, new AddOnService("Breakfast", 500));
+        manager.addService(reservationId, new AddOnService("Spa", 1500));
+
+        manager.showServices(reservationId);
+
+        System.out.println("Total Add-on Cost: " +
+                manager.calculateTotal(reservationId));
     }
 }
