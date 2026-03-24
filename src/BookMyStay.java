9c68d5f3db0caa8a194817b54be25fcd0ac92f91
megha kadiyala <meghakadiyala12@gmail.com>
2026-03-24 08:55:43 +0530

    megha kadiyala added UC3

--- src/BookMyStay.java
+++ src/BookMyStay.java
@@ -1,15 +1,56 @@
-//version 1.0
-//usecase 1:  Application Entry & Welcome Message
-public class HotelApp {
+//version 3.0
+//usecase 3:  Centralized Room Inventory (HashMap)
+import java.util.HashMap;
+import java.util.Map;
 
+class RoomInventory {
+    private Map<String, Integer> inventory;
+
+    public RoomInventory() {
+        inventory = new HashMap<>();
+git add
+        inventory.put("Single Room", 5);
+        inventory.put("Double Room", 3);
+        inventory.put("Suite Room", 2);
+    }
+
+    public int getAvailability(String roomType) {
+        return inventory.getOrDefault(roomType, 0);
+    }
+
+    public void updateAvailability(String roomType, int change) {
+        int current = inventory.getOrDefault(roomType, 0);
+        int updated = current + change;
+
+        if (updated < 0) {
+            System.out.println("⚠ Cannot reduce below 0 for " + roomType);
+            return;
+        }
+
+        inventory.put(roomType, updated);
+    }
+
+    public void displayInventory() {
+        System.out.println("\n=== Inventory ===");
+        for (Map.Entry<String, Integer> entry : inventory.entrySet()) {
+            System.out.println(entry.getKey() + " -> Available: " + entry.getValue());
+        }
+    }
+}
+
+public class UseCase3App {
     public static void main(String[] args) {
 
-        System.out.println("=================================");
-        System.out.println(" Welcome to Hotel Booking System ");
-        System.out.println(" Version: 1.0 ");
-        System.out.println("=================================");
+        RoomInventory inventory = new RoomInventory();
 
-        System.out.println("Application started successfully.");
-        System.out.println("Application terminated.");
+        inventory.displayInventory();
+
+        // Simulate booking
+        inventory.updateAvailability("Single Room", -1);
+
+        // Simulate cancellation
+        inventory.updateAvailability("Suite Room", +1);
+
+        inventory.displayInventory();
     }
 }
