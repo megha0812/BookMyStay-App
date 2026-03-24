0a0a314763506381c5c319356cdc60a3112fd273
megha kadiyala <meghakadiyala12@gmail.com>
2026-03-24 10:17:34 +0530

    megha kadiyala added UC6

--- src/BookMyStay.java
+++ src/BookMyStay.java
@@ -1,15 +1,67 @@
-//version 1.0
-//usecase 1:  Application Entry & Welcome Message
-public class HotelApp {
+//version 6.0
+//usecase 6:  Reservation Confirmation & Room Allocation
+import java.util.*;
 
+class InventoryService {
+    Map<String, Integer> inventory = new HashMap<>();
+
+    public InventoryService() {
+        inventory.put("Deluxe", 2);
+        inventory.put("Standard", 2);
+    }
+
+    public boolean isAvailable(String type) {
+        return inventory.getOrDefault(type, 0) > 0;
+    }
+
+    public void reduceInventory(String type) {
+        inventory.put(type, inventory.get(type) - 1);
+    }
+}
+
+class BookingService {
+    private Set<String> allocatedRooms = new HashSet<>();
+    private Map<String, Set<String>> roomMap = new HashMap<>();
+    private InventoryService inventory;
+
+    public BookingService(InventoryService inventory) {
+        this.inventory = inventory;
+    }
+
+    public void processBooking(Queue<Reservation> queue) {
+        while (!queue.isEmpty()) {
+            Reservation r = queue.poll();
+
+            if (inventory.isAvailable(r.roomType)) {
+                String roomId = UUID.randomUUID().toString();
+
+                allocatedRooms.add(roomId);
+
+                roomMap.putIfAbsent(r.roomType, new HashSet<>());
+                roomMap.get(r.roomType).add(roomId);
+
+                inventory.reduceInventory(r.roomType);
+
+                System.out.println("Booking Confirmed for " + r.guestName +
+                        " RoomID: " + roomId);
+            } else {
+                System.out.println("No rooms available for " + r.guestName);
+            }
+        }
+    }
+}
+
+public class UseCase6 {
     public static void main(String[] args) {
+        Queue<Reservation> queue = new LinkedList<>();
 
-        System.out.println("=================================");
-        System.out.println(" Welcome to Hotel Booking System ");
-        System.out.println(" Version: 1.0 ");
-        System.out.println("=================================");
+        queue.offer(new Reservation("Alice", "Deluxe"));
+        queue.offer(new Reservation("Bob", "Deluxe"));
+        queue.offer(new Reservation("Charlie", "Deluxe"));
 
-        System.out.println("Application started successfully.");
-        System.out.println("Application terminated.");
+        InventoryService inventory = new InventoryService();
+        BookingService service = new BookingService(inventory);
+
+        service.processBooking(queue);
     }
 }
